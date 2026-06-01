f843b09936409a4c1df96ecb4f214aede56c64b7
shebetov <flex210655@gmail.com>
2026-06-01 17:12:47 +0000

    Fix TransferMatchGroupable include order so it overrides AutoTransferMatchable

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,5 +1,5 @@
 class Family < ApplicationRecord
-  include Syncable, AutoTransferMatchable, TransferMatchGroupable, Subscribeable, VectorSearchable
+  include Syncable, TransferMatchGroupable, AutoTransferMatchable, Subscribeable, VectorSearchable
   include PlaidConnectable, SimplefinConnectable, LunchflowConnectable, AkahuConnectable, EnableBankingConnectable
   include CoinbaseConnectable, BinanceConnectable, KrakenConnectable, CoinstatsConnectable, SnaptradeConnectable, MercuryConnectable, BrexConnectable, SophtronConnectable
   include IndexaCapitalConnectable, IbkrConnectable
